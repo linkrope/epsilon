05b985b22ed3bfb89dee9dadf7c8d6a43af470bf
linkrope <linkrope@github.com>
2021-01-24 19:08:59 +0100

    Fix style

--- src/soag/eASets.d
+++ src/soag/eASets.d
@@ -128,7 +128,8 @@ void Delete(ref ASet S, int e)
 {
     if (e <= S.Max)
     {
-        int last = S.List.Last;
+        const last = S.List.Last;
+
         if (e <= last)
         {
             if (S.List.Elem[e] == e)
@@ -139,16 +140,16 @@ void Delete(ref ASet S, int e)
                 }
                 else // e < last
                 {
-                    if (S.List.Elem[last] == last) 
+                    if (S.List.Elem[last] == last)
                     {
                         S.List.Elem[e] = last;
                         S.List.Elem[last] = e;
                     }
-                    else 
-                    { 
+                    else
+                    {
                         S.List.Elem[e] = S.List.Elem[last];
                         S.List.Elem[S.List.Elem[last]] = e;
-                        S.List.Elem[last] = noelem;  
+                        S.List.Elem[last] = noelem;
                     }
                 }
                 --S.List.Last;
@@ -164,7 +165,7 @@ void Delete(ref ASet S, int e)
                     S.List.Elem[S.List.Elem[e]] = last;
                     S.List.Elem[last] = S.List.Elem[e];
                 }
-                else 
+                else
                 {
                     S.List.Elem[S.List.Elem[e]] = S.List.Elem[last];
                     S.List.Elem[S.List.Elem[last]] = S.List.Elem[e];
@@ -203,35 +204,38 @@ bool In(ASet S, int Elem)
 @("validate inserting 0 to reset set")
 unittest
 {
-    int j;
     ASet set;
+
     New(set, 10);
-    for(j = firstIndex; j < 10; ++j)
+    for(int j = firstIndex; j < 10; ++j)
         Insert(set, j);
     Reset(set);
     Insert(set, 0);
-    assert(IsEmpty(set)==false);
+
+    assert(!IsEmpty(set));
 }
 
 @("reproduce set deletion error in single-sweep.eag computation")
 unittest
 {
     ASet set1, set2;
+
     New(set1, 3);
-    
+
     Insert(set1, 0);
     Insert(set1, 2);
 
     Delete(set1, 2);
     Delete(set1, 0);
 
-    assert(IsEmpty(set1)==true);
+    assert(IsEmpty(set1));
+
     Insert(set1, 0);
-    
-    assert(IsEmpty(set1)==false);
+
+    assert(!IsEmpty(set1));
 
     New(set2, 3);
-    
+
     Insert(set2, 0);
     Insert(set2, 2);
 
@@ -240,21 +244,12 @@ unittest
     Delete(set2, 2);
 
     Insert(set2, 0);
-    
-    assert(IsEmpty(set2)==false);
+
+    assert(!IsEmpty(set2));
 
 }
 
-private void printSet(string setName, ASet set) {
-    import std;
-    writeln(setName, ", last: ", set.List.Last);
-    for(int j = firstIndex; j <= set.Max; ++j)
-        write(set.List.Elem[j], ",");
-    writeln;
-
-}
-
-@("Delete: Given {0,1,2} as [0,1,2] with last=2, delete 0 must result in {2,1} as [2,1,0]")
+@("Delete: Given {0,1,2} as [0,1,2] with last = 2, delete 0 must result in {2,1} as [2,1,0]")
 unittest
 {
     ASet set;
@@ -264,16 +259,16 @@ unittest
     Insert(set, 2);
 
     Delete(set, 0);
-    
-    assert(In(set, 0) == false);
-    assert(In(set, 1) == true);
-    assert(In(set, 2) == true);
+
+    assert(!In(set, 0));
+    assert(In(set, 1));
+    assert(In(set, 2));
 
     assert(set.List.Elem[0] == 2);
     assert(set.List.Elem[1] == 1);
 }
 
-@("Delete: Given {0,1,3} as [0,1,3,2] with last=2, delete 0 must result in {3,1} as [3,1,-1,0]")
+@("Delete: Given {0,1,3} as [0,1,3,2] with last = 2, delete 0 must result in {3,1} as [3,1,-1,0]")
 unittest
 {
     ASet set;
@@ -283,17 +278,17 @@ unittest
     Insert(set, 3);
 
     Delete(set, 0);
-    
-    assert(In(set, 0) == false);
-    assert(In(set, 1) == true);
-    assert(In(set, 2) == false);
-    assert(In(set, 3) == true);
+
+    assert(!In(set, 0));
+    assert(In(set, 1));
+    assert(!In(set, 2));
+    assert(In(set, 3));
 
     assert(set.List.Elem[0] == 3);
     assert(set.List.Elem[1] == 1);
 }
 
-@("Delete: Given {3,1,2} as [3,1,2,0] with last=2, delete 3 must result in {2,1} as [2,1,0,-1]")
+@("Delete: Given {3,1,2} as [3,1,2,0] with last = 2, delete 3 must result in {2,1} as [2,1,0,-1]")
 unittest
 {
     ASet set;
@@ -303,17 +298,17 @@ unittest
     Insert(set, 2);
 
     Delete(set, 3);
-    
-    assert(In(set, 0) == false);
-    assert(In(set, 1) == true);
-    assert(In(set, 2) == true);
-    assert(In(set, 3) == false);
+
+    assert(!In(set, 0));
+    assert(In(set, 1));
+    assert(In(set, 2));
+    assert(!In(set, 3));
 
     assert(set.List.Elem[0] == 2);
     assert(set.List.Elem[1] == 1);
 }
 
-@("Delete: Given {4,1,3} as [4,1,3,2,0] with last=2, delete 4 must result in {3,1} as [3,1,-1,0,-1]")
+@("Delete: Given {4,1,3} as [4,1,3,2,0] with last = 2, delete 4 must result in {3,1} as [3,1,-1,0,-1]")
 unittest
 {
     ASet set;
@@ -323,12 +318,12 @@ unittest
     Insert(set, 3);
 
     Delete(set, 4);
-    
-    assert(In(set, 0) == false);
-    assert(In(set, 1) == true);
-    assert(In(set, 2) == false);
-    assert(In(set, 3) == true);
-    assert(In(set, 4) == false);
+
+    assert(!In(set, 0));
+    assert(In(set, 1));
+    assert(!In(set, 2));
+    assert(In(set, 3));
+    assert(!In(set, 4));
 
     assert(set.List.Elem[0] == 3);
     assert(set.List.Elem[1] == 1);
@@ -339,14 +334,16 @@ unittest
 {
     ASet set;
     const elemCount = 10;
+
     New(set, elemCount);
 
-    for(int j = firstIndex; j <= set.Max; ++j) {
+    for(int j = firstIndex; j <= set.Max; ++j)
         Insert(set, j);
-    }
 
     assert(set.List.Last == elemCount);
-    for(int j = firstIndex; j <= set.List.Last; ++j) {
+
+    for(int j = firstIndex; j <= set.List.Last; ++j)
+    {
         assert(In(set, j));
         assert(set.List.Elem[j] == j);
     }
@@ -355,100 +352,105 @@ unittest
 @("Insert elements continuously increasing, even only")
 unittest
 {
-    import std.conv : to;
+    import std.format : format;
 
     ASet set;
     const elemCount = 11;
+
     New(set, elemCount);
 
-    for(int j = firstIndex; j <= set.Max; j=j+2) {
+    for(int j = firstIndex; j <= set.Max; j = j + 2)
         Insert(set, j);
-    }
 
-    assert(set.List.Last == elemCount/2);
-    for(int j = firstIndex; j <= set.List.Last; ++j) {
-        assert(In(set, j*2), "failed for element " ~ to!string(j*2));
-        assert(!In(set, j*2+1), "failed for element " ~ to!string(j*2+1));
-        assert(In(set, set.List.Elem[j]), "failed for element " ~ to!string(j));
+    assert(set.List.Last == elemCount / 2);
+    for(int j = firstIndex; j <= set.List.Last; ++j)
+    {
+        assert(In(set, j * 2), format!"failed for element %s"(j * 2));
+        assert(!In(set, j * 2 + 1), format!"failed for element %s"(j * 2 + 1));
+        assert(In(set, set.List.Elem[j]), format!"failed for element %s"(j));
     }
 }
 
 @("Insert elements continuously decreasing by 1")
 unittest
 {
-    import std.conv : to;
+    import std.format : format;
 
     ASet set;
     const elemCount = 10;
+
     New(set, elemCount);
 
-    for(int j = set.Max; j >= firstIndex; --j) {
+    for(int j = set.Max; j >= firstIndex; --j)
         Insert(set, j);
-    }
 
     assert(set.List.Last == elemCount);
-    for(int j = firstIndex; j <= set.List.Last; ++j) {
-        assert(In(set, j), "failed for element " ~ to!string(j*2));
-        assert(In(set, set.List.Elem[j]), "failed for element " ~ to!string(j));
+    for(int j = firstIndex; j <= set.List.Last; ++j)
+    {
+        assert(In(set, j), format!"failed for element %s"(j));
+        assert(In(set, set.List.Elem[j]), format!"failed for element %s"(j));
     }
 }
-
-
 
 @("Delete elements continuously increasing by 1")
 unittest
 {
-    import std.conv : to;
+    import std.format : format;
 
     ASet set;
     const elemCount = 10;
+
     New(set, elemCount);
 
-    for(int j = firstIndex; j <= set.Max; ++j) {
+    for(int j = firstIndex; j <= set.Max; ++j)
         Insert(set, j);
-    }
 
-    for(int j = firstIndex; j <= set.Max; ++j) {
+    for(int j = firstIndex; j <= set.Max; ++j)
+    {
         Delete(set, j);
-        for(int k = j+1; k <= set.Max; k++) {
-            assert(In(set, k), "In(set, " ~ to!string(k) ~ ") unexectedly");
+        for(int k = j+1; k <= set.Max; k++)
+        {
+            assert(In(set, k), format!"In(set, %s) unexpectedly"(k));
         }
     }
 
     assert(IsEmpty(set), "set is unexpectedly not empty");
     assert(set.List.Last == -1, "List.Last is unexpectedly not -1");
 
-    for(int j = firstIndex; j < set.Max; ++j) {
-        assert(set.List.Elem[j] == noelem, "List.Elem[" ~ to!string(j) ~ "] is not noelem");
+    for(int j = firstIndex; j < set.Max; ++j)
+    {
+        assert(set.List.Elem[j] == noelem, format!"List.Elem[%s] is not noelem"(j));
     }
 }
-
 
 @("Delete elements continuously decreasing by 1")
 unittest
 {
-    import std.conv : to;
+    import std.format : format;
 
     ASet set;
     const elemCount = 10;
+
     New(set, elemCount);
 
-    for(int j = firstIndex; j <= set.Max; ++j) {
+    for(int j = firstIndex; j <= set.Max; ++j)
         Insert(set, j);
-    }
 
-    for(int j = set.Max; j >= firstIndex; --j) {
+    for(int j = set.Max; j >= firstIndex; --j)
+    {
         Delete(set, j);
-        for(int k = firstIndex; k < j; k++) {
-            assert(In(set, k), "failed for element " ~ to!string(k));
-            assert(In(set, set.List.Elem[k]), "failed for element " ~ to!string(k));
+        for(int k = firstIndex; k < j; k++)
+        {
+            assert(In(set, k), format!"failed for element %s"(k));
+            assert(In(set, set.List.Elem[k]), format!"failed for element %s"(k));
         }
     }
 
     assert(IsEmpty(set), "set is unexpectedly not empty");
     assert(set.List.Last == -1, "List.Last is unexpectedly not 0");
 
-    for(int j = firstIndex; j < set.Max; ++j) {
-        assert(set.List.Elem[j] == noelem, "List.Elem[" ~ to!string(j) ~ "] is not noelem");
+    for(int j = firstIndex; j < set.Max; ++j)
+    {
+        assert(set.List.Elem[j] == noelem, format!"List.Elem[%s] is not noelem"(j));
     }
 }
